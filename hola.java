2f6c1608579b747336b9efcce3ec4381e4b7a566
belen <belenyamileagmail.com>
2022-08-21 20:39:03 -0500

    segundo cambio

--- hola.java
+++ hola.java
@@ -1,5 +1,6 @@
 public class hola {
   public static void main(String[] args) {
     System.out.println("Hola Diseño de Sistemas");
+    System.out.println("Cambio dos para mostrar las diferencias");
   }
 }
